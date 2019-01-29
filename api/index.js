d5aded396a6752419375ac8ed27c9dd7140ac46b
ztm1762530@163.com <ztm1762530@163.com>
2019-01-29 13:25:39 +0800

    3

--- api/index.js
+++ api/index.js
@@ -1,12 +1,8 @@
 ﻿import axios from 'axios';
 import {Notice} from 'iview'
 
-<<<<<<< HEAD
-axios.defaults.baseURL = 'http://101.132.104.74:7072/api'
-=======
 
 axios.defaults.baseURL =  process.env.baseUrl
->>>>>>> 8869d400d5d7a0701342b14c44ab4fdb7e5d8cd3
 axios.defaults.timeout = 10000
 // http request 拦截器
 axios.interceptors.request.use(
